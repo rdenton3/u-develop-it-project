eb5a90b55de542f28666507a33183367ae694b07
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-02-07 15:22:10 -0500

    separated api routes

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+// Connect to database
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    // console.log('Connected to the election database.')
+  );
+
+  
+module.exports = db;
